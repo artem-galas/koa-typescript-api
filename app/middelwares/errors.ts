7ba1838ad09463d5ac1e6f8745290161a1c66db3
artem-galas <artem.galas@gmail.com>
2017-06-21 16:12:11 +0300

    fixed error middelware

--- app/middelwares/errors.ts
+++ app/middelwares/errors.ts
@@ -1,22 +1,20 @@
 import * as Koa from 'koa';
+import {RenderCtx} from '../libs/render.class';
 
 export = async (ctx: Koa.Context, next) => {
   try {
     await next();
   } catch (e) {
     const errors = new Array();
-    if (e.status) {
-      if (e.name === 'ValidationError') {
-        for (const key in e.errors) {
-          if (e.errors.hasOwnProperty(key)) {
-            errors.push(e.errors[key].message);
-          }
+    if (e.name === 'ValidationError') {
+      for (const key in e.errors) {
+        if (e.errors.hasOwnProperty(key)) {
+          errors.push(e.errors[key].message);
         }
-        ctx.body = {
-          success: false,
-          errors: errors,
-        };
       }
+      const renderCtx = new RenderCtx();
+      renderCtx.renderFaild(ctx, 400, 'error', errors);
+    } else if (e.status) {
       ctx.status = e.statusCode || e.status || 500;
       console.log(e.message, e.stack);
     } else {
